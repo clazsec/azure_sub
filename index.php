215e60bb874c095917c3213bc88df4d062e3b258
clazsec <58617117+clazsec@users.noreply.github.com>
2019-12-08 06:36:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
  <h1>Register here!</h1>
  <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
  <form method="post" action="index.php" enctype="multipart/form-data" >
-      <table border="0" style="center; color:#FF0000">
+      <table border="0" style="center; >
        <tr>
     <td>Name </td>
     <td>:</td>
